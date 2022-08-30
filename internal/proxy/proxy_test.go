d8cc2d5a5a15e31f8cd16ca218bbd8b944b13cf4
Ten Thousand Leaves <69466447+soothing-rain@users.noreply.github.com>
2022-08-30 19:59:11 +0800

    Squash (#25)
    * Refine add segment logic for bulk load
    
    Also restructured `addNewSegment`, `addNormalSegment` and `addFlushedSegment` into a single `addSegment` to avoid duplicate code block and to see more clearly how adding these segments are different.
    
    /kind improvement
    
    Signed-off-by: Yuchen Gao <yuchen.gao@zilliz.com>
    
    * Add garbage collection function for segments produced by partially failed imports (#6)
    
    Signed-off-by: xingzhao <xing.zhao@zilliz.com>
    
    Co-authored-by: xingzhao <xing.zhao@zilliz.com>
    
    * Enhance error handle in bulk load (#5)
    
    Signed-off-by: wayblink <anyang.wang@zilliz.com>
    
    * Refine expiration/retention policy for import tasks (#9)
    
    * Persist import task info to meta before update in memory (#12)
    
    Signed-off-by: wayblink <anyang.wang@zilliz.com>
    
    * Add start position for import segment (#7)
    
    Signed-off-by: wayblink <anyang.wang@zilliz.com>
    
    * Refine bulkload proto (#11)
    
    Signed-off-by: groot <yihua.mo@zilliz.com>
    
    * Remove queryable/indexed logic and some other refinement (#14)
    
    Signed-off-by: Yuchen Gao <yuchen.gao@zilliz.com>
    
    * Resolve conflicts
    
    Signed-off-by: Yuchen Gao <yuchen.gao@zilliz.com>
    
    * Some minor tweaks
    
    Signed-off-by: Yuchen Gao <yuchen.gao@zilliz.com>
    
    * Implement CompleteBulkLoad feature (#19)
    
    * Update interface and fix bug
    
    Signed-off-by: Yuchen Gao <yuchen.gao@zilliz.com>
    
    * Fix dml start position mistake in AddSegmentReq (#21)
    
    Signed-off-by: wayblink <anyang.wang@zilliz.com>
    
    Signed-off-by: wayblink <anyang.wang@zilliz.com>
    
    * Fix getDmlChannelPositionByBroadcast (#23)
    
    * Fix null start position issue
    
    Signed-off-by: Yuchen Gao <yuchen.gao@zilliz.com>
    
    * Fix merge conflicts
    
    Signed-off-by: Yuchen Gao <yuchen.gao@zilliz.com>
    
    * Fix conflicts
    
    Signed-off-by: Yuchen Gao <yuchen.gao@zilliz.com>
    
    * Bulkload enhancement (#24)
    
    Signed-off-by: yhmo <yihua.mo@zilliz.com>
    
    * Add some unit tests
    
    Signed-off-by: Yuchen Gao <yuchen.gao@zilliz.com>
    
    Signed-off-by: Yuchen Gao <yuchen.gao@zilliz.com>
    Signed-off-by: wayblink <anyang.wang@zilliz.com>
    Signed-off-by: groot <yihua.mo@zilliz.com>
    Signed-off-by: yhmo <yihua.mo@zilliz.com>
    Co-authored-by: xing.zhao <34002927+EricStarer@users.noreply.github.com>
    Co-authored-by: xingzhao <xing.zhao@zilliz.com>
    Co-authored-by: wayblink <anyang.wang@zilliz.com>
    Co-authored-by: groot <yihua.mo@zilliz.com>

--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -414,6 +414,8 @@ func TestProxy(t *testing.T) {
 	localMsg := true
 	factory := dependency.NewDefaultFactory(localMsg)
 	alias := "TestProxy"
+	CheckTaskPersistedInterval = 10 * time.Millisecond
+	CheckTaskPersistedWaitLimit = 100 * time.Millisecond
 
 	Params.InitOnce()
 	log.Info("Initialize parameter table of Proxy")
@@ -1617,6 +1619,8 @@ func TestProxy(t *testing.T) {
 		resp, err := proxy.Import(context.TODO(), req)
 		assert.EqualValues(t, commonpb.ErrorCode_Success, resp.Status.ErrorCode)
 		assert.Nil(t, err)
+		// Wait a bit for complete import to start.
+		time.Sleep(2 * time.Second)
 	})
 
 	wg.Add(1)
